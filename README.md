dae3e8667ed5dd7f0fa5cc80af7e0065408a78c4
Bruno D'Luka <brunodlukaa@gmail.com>
2024-05-07 10:01:12 -0300

    feat: Organize project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # de.gustar
  Find the perfect restaurant for you hunger
+
+This is a project for the course "Software Engineering" at the CEUB.
---
+++ lib/screens/root.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatelessWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('de.gustar'),
+      ),
+    );
+  }
+}
